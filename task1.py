ec26bd412ad21960ca6da26f628c9c16602064b8
AlenVinu <alenvinu999@gmail.com>
2024-06-05 11:54:44 +0530

    python basic

---
+++ task1.py
@@ -0,0 +1,16 @@
+my_list=[1,2,3,4,5,6]
+my_list.append(7)
+my_list.remove(5)
+my_list[0]
+print(my_list)
+my_dict={'name':'jungkook','age':'27','city':'goa'}
+my_dict['gender']='male'
+del my_dict['age']
+my_dict['city']='Bangalore'
+print('updated dictonary:',my_dict)
+my_set={1,2,3,4,5,6}
+my_set.add(7)
+my_set.remove(3)
+my_set.discard(2)
+my_set.add(10)
+print('updated set:',my_set)
